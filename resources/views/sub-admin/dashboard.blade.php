3be658103d37c47ae240d29ef36a68181a06ff68
mudpirates04 <developerwebsite04@gmail.com>
2024-11-17 23:46:55 +0500

    set the basic login screen and register screen with there backend and also make the teacher dashboard

---
+++ resources/views/sub-admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+@section('title','Manager Dashboard')
+@section('body')
+<h2 class="mb-4">Manager Dashbaord</h2>
+<a href="/logout">Logout</a>
+@endsection
+{{-- @section('space-work')
+
+    
+
+@endsection --}}
